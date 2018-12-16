fe725ea240a7c7db070f2122e2265cf4349b74e5
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-12-16 10:12:18 +0100

    Components

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from '../App';
+
+it('shows a comment box', () => {
+  const div = document.createElement('div');
+
+  ReactDom.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Comment Box');
+
+  ReactDom.unmountComponentAtNode(div);
+});
