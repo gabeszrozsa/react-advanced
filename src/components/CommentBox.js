811e4e895f1e65ae0086cc9b19d5f493d6258735
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-12-16 17:44:32 +0100

    absolute path, CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+export default class CommentBox extends Component {
+  state = { comment: '' };
 
-export default () => {
-  return (
-    <div>Comment Box</div>
-  )
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment}/>
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
 }
