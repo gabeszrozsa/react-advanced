cf5b1b25a1d2bd3b822523127c4e33bc67d7ed6f
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2018-12-17 23:25:36 +0100

    Fetch Comments

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
 
 const defaultState = [];
 
@@ -6,6 +6,9 @@ export default function(state = defaultState, action) {
   switch (action.type) {
     case SAVE_COMMENT:
       return [...state, action.payload];
+    case FETCH_COMMENTS:
+      const comments = action.payload.data.map(comment => comment.name);
+      return [...state, ...comments];
     default:
       return state;
   }
